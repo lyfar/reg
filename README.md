4e0029680f1ab8aba8c2470ad1952ca72ec111dd
Egor <egor.lyfar@gmail.com>
2024-04-08 16:45:31 +0800

    init

---
+++ README.md
@@ -0,0 +1,9 @@
+## Login Feature Technical Specification for ZFX PWA - made with Lumentis
+
+`curl -fsSL https://bun.sh/install | bash # Install bun for macOS, Linux, and WSL`
+
+`bun install`
+
+`bun dev`
+
+Change things in `pages` to see the effect.
---
+++ theme.config.jsx
@@ -0,0 +1,31 @@
+export default {
+      logo: <span>Login Feature Technical Specification for ZFX PWA - made with Lumentis</span>,
+      editLink: {
+        component: null,
+      },
+      project: {
+        link: "https://github.com/hrishioa/lumentis",
+      },
+      feedback: {
+        content: null,
+      },
+      footer: {
+        text: (
+          <>
+            Made with 🫶 by&nbsp;
+            <a href="https://twitter.com/hrishioa" target="_blank">
+              Hrishi - say hi!
+            </a>
+          </>
+        ),
+      },
+      head: (
+        <>
+          <meta property="og:title" content="Login Feature Technical Specification for ZFX PWA" />
+          <meta property="og:description" content="The provided text appears to be a technical design document or specification for the login functionality and user interface of a Progressive Web App (PWA) for a trading platform called ZFX. It outlines the steps and features of the login process, including language selection, phone and email login options, password recovery, and registration redirection. The document is likely written by a product manager or technical lead for the development and QA teams working on the ZFX trading app, with a focus on catering to a primarily Chinese user base while supporting other regions as well." />
+          <meta name="robots" content="noindex, nofollow" />
+          <link rel="icon" type="image/x-icon" href="https://raw.githubusercontent.com/HebeHH/lumentis/choose-favicon/assets/default-favicon.png" />
+        </>
+      ),
+    };
+    
